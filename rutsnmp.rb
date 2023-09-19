a3dce161a30fb98bab97b3bdc9fc70aa8ff95844
Martin Pugh <pugh@hey.com>
2023-09-19 15:41:17 +0100

    fix indentation

--- rutsnmp.rb
+++ rutsnmp.rb
@@ -26,20 +26,20 @@ url = ENV['INFLUX_URL']
 
 client = InfluxDB2::Client.new url, token, bucket: bucket, org: org, precision: 'InfluxDB2::WritePrecision::NANOSECOND'
 
-  point = InfluxDB2::Point.new(name: 'rutsnmp')
-  point.add_tag('host', 'rutsnmp')
-  point.add_field 'mSignal', manager.get(oid: oids[:mSignal])
-  point.add_field 'mConnectionType', manager.get(oid: oids[:mConnectionType])
-  point.add_field('mSent', manager.get(oid: oids[:mSent]) & 0xFFFFFFFF)
-  point.add_field('mReceived', manager.get(oid: oids[:mReceived]) & 0xFFFFFFFF)
-  puts '---------------------'
-  puts point.to_line_protocol
-  puts '---------------------'
+point = InfluxDB2::Point.new(name: 'rutsnmp')
+point.add_tag('host', 'rutsnmp')
+point.add_field 'mSignal', manager.get(oid: oids[:mSignal])
+point.add_field 'mConnectionType', manager.get(oid: oids[:mConnectionType])
+point.add_field('mSent', manager.get(oid: oids[:mSent]) & 0xFFFFFFFF)
+point.add_field('mReceived', manager.get(oid: oids[:mReceived]) & 0xFFFFFFFF)
+puts '---------------------'
+puts point.to_line_protocol
+puts '---------------------'
 
-  begin
+begin
   write_options = InfluxDB2::WriteOptions.new(write_type: InfluxDB2::WriteType::BATCHING, batch_size: 10, flush_interval: 5_000, max_retries: 3, max_retry_delay: 15_000, exponential_base: 2)
   write_api = client.create_write_api(write_options: write_options)
   write_api.write(data: point)
-  rescue InfluxDB2::InfluxError => e
-    puts "InfluxDB exception: #{e}"
-  end
+rescue InfluxDB2::InfluxError => e
+  puts "InfluxDB exception: #{e}"
+end
